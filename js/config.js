b851ed972ebf48dc5d25e4b5a5e8bf13b97f64c3
JayM647 <jemarmrrs@gmail.com>
2023-08-26 20:50:05 -0400

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_2LjJbzAcQ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5g4j388q02orpg7nep946vt8eu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
